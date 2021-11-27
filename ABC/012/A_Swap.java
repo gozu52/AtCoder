d8b286c6d7181269168213e025e5e1711b6c56a7
gozu52 <bknb0225@gmail.com>
2021-11-27 19:59:14 +0900

    A

---
+++ ABC/012/A_Swap.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A_Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        sc.close();
+        System.out.println(b+" "+a);
+    }
+}
