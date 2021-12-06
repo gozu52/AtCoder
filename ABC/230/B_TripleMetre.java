20a4e486d2ea15ed441e899babe81e4899700567
gozu52 <bknb0225@gmail.com>
2021-12-07 00:28:33 +0900

    B

---
+++ ABC/230/B_TripleMetre.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class B_TripleMetre {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+        String ans1 ="oxxoxxoxxoxxoxxoxxox";
+        String ans2 ="xxoxxoxxoxxoxxoxxoxx";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+
+            }
+        }
+        String ans = "No";
+        if(ans1.contains(s))ans = "Yes";
+        if(ans2.contains(s))ans = "Yes";
+        System.out.println(ans);
+    }   
+}
