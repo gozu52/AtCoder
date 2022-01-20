5b2cdb168bd890e012c22ab0dc08c84f6b10a4d4
gozu52 <bknb0225@gmail.com>
2022-01-20 19:32:42 +0900

    add 001

---
+++ MathAlgo/Chapter2/q001.java
@@ -0,0 +1,12 @@
+package MathAlgo.Chapter2;
+
+import java.util.*;
+
+public class q001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println((n+5));
+    }
+}
