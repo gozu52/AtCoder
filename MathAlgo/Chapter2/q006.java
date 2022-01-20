9e4cc28165c8bc211bd2eee9ea7a29a7959b7cd9
gozu52 <bknb0225@gmail.com>
2022-01-20 19:40:05 +0900

    add 006

---
+++ MathAlgo/Chapter2/q006.java
@@ -0,0 +1,13 @@
+package MathAlgo.Chapter2;
+
+
+import java.util.Scanner;
+
+public class q006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println((2*n)+3);
+    }
+}
