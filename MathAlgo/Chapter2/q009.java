2a648d3862e2159f660461e43f307b12ac72ae72
gozu52 <bknb0225@gmail.com>
2022-01-20 19:40:21 +0900

    add 009

---
+++ MathAlgo/Chapter2/q009.java
@@ -0,0 +1,14 @@
+package MathAlgo.Chapter2;
+
+import java.util.*;
+
+public class q009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)a[i]=sc.nextInt();
+        sc.close();
+
+    }
+}
