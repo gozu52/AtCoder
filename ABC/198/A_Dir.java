d99420d0fcd0d5a6b20084959d3e4dddb32d409c
gozu52 <bknb0225@gmail.com>
2022-02-04 16:01:42 +0900

    add 198 a

---
+++ ABC/198/A_Dir.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class A_Dir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(n-1);
+    }
+}
