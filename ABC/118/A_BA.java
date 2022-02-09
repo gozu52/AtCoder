cc0cc7d4d5435b3aba0d938e6ccb616b861c2b87
gozu52 <bknb0225@gmail.com>
2022-02-09 20:22:49 +0900

    add 118 a

---
+++ ABC/118/A_BA.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A_BA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        sc.close();
+        if(b%a==0)System.out.println(a+b);
+        else System.out.println(b-a);
+    }
+}
