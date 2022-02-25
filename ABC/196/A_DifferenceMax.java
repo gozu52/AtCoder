a8fc54171510db98064c68c4cc236e8959a1eed4
gozu52 <bknb0225@gmail.com>
2022-02-25 23:38:03 +0900

    add abc196 a

---
+++ ABC/196/A_DifferenceMax.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A_DifferenceMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int c = sc.nextInt(), d = sc.nextInt();
+        sc.close();
+        System.out.println(b-c);
+    }
+}
